44f032a34af43d3870a9534a3761c19b4e6880e6
Theodore Martin <theodore.martin1@protonmail.com>
2023-03-17 15:06:35 +0100

    added the pseudo random generator

---
+++ libs/random.h
@@ -0,0 +1,9 @@
+#ifndef __K_RANDOM__
+#define __K_RANDOM__
+#include "utils.h"
+/*returns a pseudo random number between 0 and 255*/
+u8 random_k();
+/*resets the pseudo random generator*/
+void resetRandom();
+
+#endif
